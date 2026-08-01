128c39d471f5167589d5dd3873a3e23af297385a
NEHAD711 <442213121@qu.edu.sa>
2026-08-01 08:49:33 +0300

    HTML forms and handling forms in Django

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('html5/text/formatting', views.text_formatting, name='text_formatting'),
     path('html5/listing', views.html5_listing, name='html5_listing'),
     path('html5/tables', views.html5_tables, name='html5_tables'),
+    path('search/', views.searchBooks, name='searchBooks'),
 ]
