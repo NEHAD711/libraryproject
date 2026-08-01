dd7e0658777a366a0d1ba58d5e8819e64aafee62
NEHAD711 <442213121@qu.edu.sa>
2026-08-01 07:39:34 +0300

    Update tables and add desert image

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('list_books/', views.list_books, name="books.list_books"),
     path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
     path('aboutus/', views.aboutus, name="books.aboutus"),
+    path('html5/links', views.html5_links, name='html5_links'),
+    path('html5/text/formatting', views.text_formatting, name='text_formatting'),
+    path('html5/listing', views.html5_listing, name='html5_listing'),
+    path('html5/tables', views.html5_tables, name='html5_tables'),
 ]
